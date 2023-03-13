61d51104fd24f702be5bad80b52148c6931402d3
Янислав Любомиров Стоянов <YLStoyanov19@codingburgas.bg>
2023-03-13 15:07:52 +0200

    Add code

---
+++ src/test/java/ChessJava/ChessJava/ChessJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ChessJava.ChessJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

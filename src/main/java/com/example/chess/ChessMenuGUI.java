c60ea273d5fb5402c56e4d0d673db7c80a853519
YLStoyanov19 <YLStoyanov19@codingburgas.bg>
2023-04-20 09:20:07 +0300

    Add main information

--- src/main/java/com/example/chess/ChessMenuGUI.java
+++ src/main/java/com/example/chess/ChessMenuGUI.java
@@ -3,9 +3,6 @@ package com.example.chess;
 import javax.swing.*;
 
 public class ChessMenuGUI extends JFrame {
-    private JMenuItem historyAndRulesMenuItem;
-    private JMenuItem playWithAIMenuItem;
-    private JMenuItem playerVsPlayerMenuItem;
 
     public ChessMenuGUI() {
         setTitle("Chess");
@@ -16,21 +13,21 @@ public class ChessMenuGUI extends JFrame {
         JMenuBar menuBar = new JMenuBar();
 
         // Create "File" menu
-        JMenu fileMenu = new JMenu("Profile");
+        JMenu fileMenu = new JMenu("Learn Chess");
 
         // Create "History and Rules of Chess" menu item
-        historyAndRulesMenuItem = new JMenuItem("History and Rules of Chess");
+        JMenuItem historyAndRulesMenuItem = new JMenuItem("History and Rules of Chess");
         fileMenu.add(historyAndRulesMenuItem);
 
         // Create "Play" menu
         JMenu playMenu = new JMenu("Play");
 
         // Create "Play with AI" menu item
-        playWithAIMenuItem = new JMenuItem("Play with AI");
+        JMenuItem playWithAIMenuItem = new JMenuItem("Player versus AI");
         playMenu.add(playWithAIMenuItem);
 
         // Create "Player versus Player" menu item
-        playerVsPlayerMenuItem = new JMenuItem("Player versus Player");
+        JMenuItem playerVsPlayerMenuItem = new JMenuItem("Player versus Player");
         playMenu.add(playerVsPlayerMenuItem);
 
         // Add "File" and "Play" menus to menu bar
@@ -46,14 +43,24 @@ public class ChessMenuGUI extends JFrame {
         playerVsPlayerMenuItem.addActionListener(e -> startPlayerVsPlayerGame());
     }
 
-    private void showHistoryAndRules() {
-        // TODO: Implement logic to display history and rules of chess
-        JOptionPane.showMessageDialog(this, "Displaying History and Rules of Chess");
-    }
-
     private void startGameWithAI() {
         // TODO: Implement logic to start a game with AI
         JOptionPane.showMessageDialog(this, "Starting Game with AI");
+    }
+
+    private void showHistoryAndRules() {
+        String historyAndRules = "History of Chess:\n"
+                + "Chess is believed to have originated in Northern India during the Gupta Empire in the 6th century AD. "
+                + "It then spread to Persia and eventually to the Islamic empire and other parts of the world.\n\n"
+                + "Rules of Chess:\n"
+                + "1. Chess is a two-player strategy game played on an 8x8 board.\n"
+                + "2. Each player starts with 16 pieces: one king, one queen, two rooks, two knights, two bishops, and eight pawns.\n"
+                + "3. The goal is to checkmate the opponent's king, which means putting the king in a position where it is under attack and cannot escape capture.\n"
+                + "4. Each piece has its own unique moves: the king moves one square in any direction, the queen can move any number of squares in any direction, the rook moves horizontally or vertically, the bishop moves diagonally, the knight moves in an L-shape, and the pawn moves forward one or two squares initially and then one square forward or diagonally to capture.\n"
+                + "5. The game ends when a player checkmates the opponent's king, stalemates the opponent's king, or the game is a draw due to a threefold repetition, insufficient material, or a draw offer.\n\n"
+                + "Have fun playing Chess!";
+
+        JOptionPane.showMessageDialog(this, historyAndRules);
     }
 
     private void startPlayerVsPlayerGame() {

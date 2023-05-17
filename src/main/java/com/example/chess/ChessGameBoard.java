1f993e41a9fbb2604836e79e77b9b16169a98afd
YLStoyanov19 <YLStoyanov19@codingburgas.bg>
2023-05-17 13:50:58 +0300

    Change code

--- src/main/java/com/example/chess/ChessGameBoard.java
+++ src/main/java/com/example/chess/ChessGameBoard.java
@@ -13,7 +13,7 @@ public class ChessGameBoard extends JFrame implements MouseListener, MouseMotion
     int xAdjustment;
     int yAdjustment;
 
-    public ChessGameBoard(){
+    public ChessGameBoard() {
         Dimension boardSize = new Dimension(600, 600);
 
         layeredPane = new JLayeredPane();
@@ -22,104 +22,87 @@ public class ChessGameBoard extends JFrame implements MouseListener, MouseMotion
         layeredPane.addMouseListener(this);
         layeredPane.addMouseMotionListener(this);
 
-        //Add a chess board to the Layered Pane
+        // Add a chess board to the Layered Pane
 
         chessBoard = new JPanel();
         layeredPane.add(chessBoard, JLayeredPane.DEFAULT_LAYER);
-        chessBoard.setLayout( new GridLayout(8, 8) );
-        chessBoard.setPreferredSize( boardSize );
+        chessBoard.setLayout(new GridLayout(8, 8));
+        chessBoard.setPreferredSize(boardSize);
         chessBoard.setBounds(0, 0, boardSize.width, boardSize.height);
 
         for (int i = 0; i < 64; i++) {
-            JPanel square = new JPanel( new BorderLayout() );
-            chessBoard.add( square );
+            JPanel square = new JPanel(new BorderLayout());
+            chessBoard.add(square);
 
             int row = (i / 8) % 2;
             if (row == 0)
-                square.setBackground( i % 2 == 0 ? Color.white : Color.darkGray );
+                square.setBackground(i % 2 == 0 ? Color.white : Color.darkGray);
             else
-                square.setBackground( i % 2 == 0 ? Color.darkGray : Color.white );
+                square.setBackground(i % 2 == 0 ? Color.darkGray : Color.white);
         }
 
-        //Add a few pieces to the board
+        // Add a few pieces to the board
 
-        JLabel piece = new JLabel( new ImageIcon("/home/vinod/resamples/chess.jpg") );
-        JPanel panel = (JPanel)chessBoard.getComponent(0);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("/home/vinod/resamples/chess1.jpg"));
-        panel = (JPanel)chessBoard.getComponent(15);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("/home/vinod/resamples/king.jpg"));
-        panel = (JPanel)chessBoard.getComponent(16);
-        panel.add(piece);
-        piece = new JLabel(new ImageIcon("/home/vinod/resamples/camel.jpg"));
-        panel = (JPanel)chessBoard.getComponent(20);
-        panel.add(piece);
+        ImageIcon chessIcon = new ImageIcon("C:\\Users\\YLStoyanov19\\IdeaProjects\\2223-otj-11-project-repo-java-YLStoyanov19\\Images\\Chess_Pieces\\black.png");
+        ImageIcon chess1Icon = new ImageIcon("/home/vinod/resamples/chess1.jpg");
+        ImageIcon kingIcon = new ImageIcon("/home/vinod/resamples/king.jpg");
+        ImageIcon camelIcon = new ImageIcon("/home/vinod/resamples/camel.jpg");
 
+        addPieceToSquare(chessIcon, 0);
+        addPieceToSquare(chess1Icon, 15);
+        addPieceToSquare(kingIcon, 16);
+        addPieceToSquare(camelIcon, 20);
     }
 
-    public void mousePressed(MouseEvent e){
-        chessPiece = null;
-        Component c =  chessBoard.findComponentAt(e.getX(), e.getY());
-
-        if (c instanceof JPanel)
-            return;
-
-        Point parentLocation = c.getParent().getLocation();
-        xAdjustment = parentLocation.x - e.getX();
-        yAdjustment = parentLocation.y - e.getY();
-        chessPiece = (JLabel)c;
-        chessPiece.setLocation(e.getX() + xAdjustment, e.getY() + yAdjustment);
-        chessPiece.setSize(chessPiece.getWidth(), chessPiece.getHeight());
-        layeredPane.add(chessPiece, JLayeredPane.DRAG_LAYER);
+    private void addPieceToSquare(ImageIcon icon, int squareIndex) {
+        JLabel piece = new JLabel(icon);
+        JPanel panel = (JPanel) chessBoard.getComponent(squareIndex);
+        panel.add(piece);
     }
 
-    //Move the chess piece around
+    // Rest of the code...
 
-    public void mouseDragged(MouseEvent me) {
-        if (chessPiece == null) return;
-        chessPiece.setLocation(me.getX() + xAdjustment, me.getY() + yAdjustment);
+    public static void main(String[] args) {
+        JFrame frame = new ChessGameBoard();
+        frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        frame.pack();
+        frame.setResizable(true);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 
-    //Drop the chess piece back onto the chess board
-
-    public void mouseReleased(MouseEvent e) {
-        if(chessPiece == null) return;
-
-        chessPiece.setVisible(false);
-        Component c =  chessBoard.findComponentAt(e.getX(), e.getY());
-
-        Container parent;
-        if (c instanceof JLabel){
-            parent = c.getParent();
-            parent.remove(0);
-        }
-        else {
-            parent = (Container) c;
-        }
-        parent.add( chessPiece );
-
-        chessPiece.setVisible(true);
-    }
-
+    @Override
     public void mouseClicked(MouseEvent e) {
 
     }
-    public void mouseMoved(MouseEvent e) {
-    }
-    public void mouseEntered(MouseEvent e){
+
+    @Override
+    public void mousePressed(MouseEvent e) {
 
     }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
     public void mouseExited(MouseEvent e) {
 
     }
 
-    public static void main(String[] args) {
-        JFrame frame = new ChessGameBoard();
-        frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE );
-        frame.pack();
-        frame.setResizable(true);
-        frame.setLocationRelativeTo( null );
-        frame.setVisible(true);
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
     }
 }
